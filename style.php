2c5c9436b187ed0e76da065313375eda449522f4
vignesh <v.vicky32@gmail.com>
2012-08-25 16:45:20 +0530

    Style file Which has all the css and js links with title

---
+++ style.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Benchmark Graph Generation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/multi-select.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap-dropdown.js"></script>
+	<script src="js/bootstrap-collapse.js"></script>
+	<script src="js/jquery.multi-select.js"></script>
+	<script src="js/jquery.quicksearch.js"></script>
+    </head>
+
+<body>
+<div class="navbar navbar-fixed-top">
+<div class="navbar-inner">
+</div></div>
+		
+<div class="container">
+	
