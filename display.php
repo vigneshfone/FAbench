9fe707931a0c188ef91ef8f3913e8c476f089391
vignesh <v.vicky32@gmail.com>
2012-08-25 13:15:40 +0530

    First commit

---
+++ display.php
@@ -0,0 +1,82 @@
+<?php
+ini_set('display_erors',1);
+include_once 'style.php';
+include_once 'config.php';
+require_once 'phplot/phplot.php';
+if(!empty($_REQUEST['searchable'])) {
+   $matches = implode(',', $_REQUEST['searchable']);
+   $data = array();
+   $higlight_phone=$_REQUEST['current_phone'];
+} else {
+echo "Slelect any phones to display chart";
+exit;
+}
+$db = new DB_Class();
+$result = mysql_query("select phone_name, quadrant from benchmarks left join phone on benchmarks.phone_id= phone.phone_id where quadrant <> 0 and benchmarks.phone_id in ($matches) order by quadrant asc");
+$n_rows = mysql_num_rows($result);
+for($i = 0; $i < $n_rows; $i++){
+ $data[] = mysql_fetch_row($result, MYSQL_NUM);
+ }
+
+ //$data=conn();
+ $plot = new PHPlot(574, 432);
+//$plot->SetIsInline(True);
+$plot->SetBgImage('watermark.png', 'scale');
+//$plot->SetPlotAreaBgImage('Veuqs.png', 'scale');
+//$plot->SetPlotAreaPixels(NULL, NULL, 574, 432);
+$plot->SetImageBorderType('solid'); // Improves presentation in the manual
+$plot->SetTitle("Samsung Galaxy S3 Quadrant Comparison");
+$plot->SetXTitle('Another subtitle', 'plotup');//modified core file line no 4500 DrawXTitle() for subtitle positioning
+$plot->SetMarginsPixels(NULL, 15, 70);
+//$plot->SetImageBorderColor('#00f0fa');
+$plot->SetBackgroundColor('#ffffff');
+$plot->SetPlotAreaWorld(0);
+$plot->SetTTFPath('./phplot');
+// No ticks along Y axis, just bar labels:
+$plot->SetYTickPos('none');
+//  No ticks along X axis:
+$plot->SetXTickPos('xaxis');
+// No X axis labels. The data values labels are sufficient.
+$plot->SetXTickLabelPos('xaxis');
+// Turn on the data value labels:
+$plot->SetXDataLabelPos('plotin');
+//$plot->SetXDataLabelAngle(90);
+$plot->SetPrecisionX(0);
+$plot->SetNumberFormat('', '');
+//$plot->SetTransparentColor('#FFCC00');
+$plot->SetFontTTF('title', 'arial.ttf', 14);
+$plot->SetFontTTF('x_title', 'arial.ttf', 12);
+$plot->SetFontTTF('y_label', 'arial.ttf', 10);
+$plot->SetFontTTF('x_label', 'arial.ttf', 8);
+$plot->SetDataValueLabelColor('#ffffff');
+//No grid lines are needed:
+$plot->SetDrawXGrid(true);
+//$plot->SetLegend(array('quadrant','antutu','example'));
+$plot->SetCallback('data_color', 'pickcolor', $data);
+// Set the bar fill color:
+$plot->SetDataColors(array('#FFCC00','#33AACC'));
+// Use less 3D shading on the bars:
+$plot->SetShading(10);
+$plot->SetDataValues($data);
+$plot->SetDataType('text-data-yx');
+//$plot->SetPlotType('stackedbars');
+$plot->SetPlotType('bars');
+$val=rand(1,10);
+//$plot->SetOutputFile('GeneratedImages/sample'.$val.'.png');
+$plot->SetPrintImage(False);
+$plot->DrawGraph(); 
+
+?>
+<div class="row">
+<div class="span12" style="text-align:center">
+<?php
+function pickcolor($img, $data_array, $row, $col)
+{
+$current_phone = $data_array[$row][0];
+if ($current_phone == $_REQUEST['current_phone']) return 0;
+return 1;
+}
+echo "<img src=\"" . $plot->EncodeImage() . "\">\n";
+?>
+</div></div>
+</div>
