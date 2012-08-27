febd748e938c4c95389b002a3c5a66ceb2b06033
vignesh <v.vicky32@gmail.com>
2012-08-27 16:10:36 +0530

    Added Ajax functionality for field names. Also we brought values using mysql_field_name().

---
+++ ajax.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_erors',1);
+
+include_once 'config.php';
+$db = new DB_Class();
+
+if($_POST['id'])
+{
+$id=$_POST['id'];
+//echo $id;
+$result = mysql_query("select benchmarks.phone_id, phone_name, $id from benchmarks left join phone on benchmarks.phone_id= phone.phone_id where $id <> 0 order by $id asc");
+$data = array();
+$n_rows = mysql_num_rows($result);
+for($i = 0; $i < $n_rows; $i++){
+ $data[] = mysql_fetch_row($result, MYSQL_NUM);
+ }
+ for($i=0;$i<count($data);$i++)	{ 
+			echo '<option value="'.$data[$i][0].'">'.$data[$i][1].'</option>';
+
+		}
+} 
+?>
--- index.php
+++ index.php
@@ -3,30 +3,29 @@ ini_set('display_erors',1);
 include_once 'config.php';
 include_once 'style.php';
 $db = new DB_Class();
-$result = mysql_query("select benchmarks.phone_id, phone_name, quadrant from benchmarks left join phone on benchmarks.phone_id= phone.phone_id where quadrant <> 0 order by quadrant asc");
-$data = array();
-$n_rows = mysql_num_rows($result);
-for($i = 0; $i < $n_rows; $i++){
- $data[] = mysql_fetch_row($result, MYSQL_NUM);
- }
+$result = mysql_query("select * from benchmarks");
+
 ?>
 <div class="hero-unit span8">
 	<h2>Benchmark Graph Generation</h2>
 	<form method="POST" action="display.php" name='display'>
 	<table class="table table-bordered table-condensed">
-		<tr><td>Select The Phones to Compare</td><td>
-			<select id='searchable' multiple='multiple' name="searchable[]">
-			<?php for($i=0;$i<count($data);$i++)	{ ?>
-			<option value="<?php echo $data[$i][0]; ?>"><?php echo $data[$i][1]; ?></option>
+	<tr><td>Select The processor</td><td>
+			<select id='processor' name="processor">
+			<option value="">-- Select Current Phone --</option>
+			 <?php for($i=2;$i<mysql_num_fields($result);$i++){ 
+			 $field= mysql_field_name($result, $i);?>
+			 <option value="<?php echo $field; ?>"><?php echo $field; ?></option>
 			<?php } ?>
+			</select>
+		<tr><td>Select The Phones to Compare</td><td>
+			<select id='searchable' multiple='multiple' name="searchable[]" class="searchable">
+			
 			</select>
 		</td></tr>
 		<tr><td>Current Phone</td><td>
 			<select name="current_phone" id="current_phone">
-			 <option value="">-- Select Current Phone --</option>
-			 <?php for($i=0;$i<count($data);$i++){ ?>
-			 <option value="<?php echo $data[$i][0]; ?>"><?php echo $data[$i][1]; ?></option>
-			<?php } ?>
+			 <option selected="selected" value="">-- Select Current Phone --</option>
 			</select>
 		</td></tr>
 		<tr><td>Enter Title</td><td>
@@ -52,9 +51,27 @@ for($i = 0; $i < $n_rows; $i++){
 
 ?>
 <script type = "text/javascript">
-$('input#graph').click(function(){
-	var current =$('#current_phone').val();
-	var phone =$('#searchable').val();
+$('#searchable').multiSelect({
+  selectableHeader: "<input type='text' id='search' autocomplete='off' placeholder='Phone name'>"
+});
+
+$('#search').quicksearch(
+  $('li', '#ms-searchable' )).on('keydown', function(e){
+    if (e.keyCode == 40){
+      $(this).trigger('focusout');
+      $('#searchable').focus();
+      return false;
+    }
+  }
+);
+
+var j = jQuery.noConflict();
+j(document).ready(function()
+{
+
+j('input#graph').click(function(){
+	var current =j('#current_phone').val();
+	var phone =j('#searchable').val();
 	var comp = phone.indexOf(current);
 	//alert(comp);
 	if(phone==null){
@@ -72,19 +89,28 @@ $('input#graph').click(function(){
 	return true;
 	}
 });
-$('#searchable').multiSelect({
-  selectableHeader: "<input type='text' id='search' autocomplete='off' placeholder='Phone name'>"
+j("#processor").change(function()
+{
+var id=j(this).val();
+//alert(id);
+j.ajax
+({
+type: "POST",
+url: "ajax.php",
+data: {'id':id},
+success: function(html)
+{
+//alert(html);
+j('select#searchable').html(html);
+j('#current_phone').html(html);
+j('select#searchable').multiSelect('refresh');
+}
 });
 
-$('#search').quicksearch(
-  $('li', '#ms-searchable' )).on('keydown', function(e){
-    if (e.keyCode == 40){
-      $(this).trigger('focusout');
-      $('#searchable').focus();
-      return false;
-    }
-  }
-);
+});
+});
+
+
 
 </script>
 </body>
